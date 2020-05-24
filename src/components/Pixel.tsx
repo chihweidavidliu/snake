ec2039f33190c2d953a08a7e61794c964ab6d9c5
David Liu <chihweiliu1993@gmail.com>
2020-05-24 21:10:26 +0100

    Snake styling

--- src/components/Pixel.tsx
+++ src/components/Pixel.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { useGameContext } from "../context/GameContext";
+import { Direction } from "../types/direction";
 
 interface IPixelWrapperProps {
   left?: number;
@@ -8,6 +9,7 @@ interface IPixelWrapperProps {
   size: number;
   isGameStarted: boolean;
   index: number;
+  direction: Direction;
 }
 
 const PixelWrapper = styled.div.attrs<IPixelWrapperProps>((props) => ({
@@ -22,6 +24,93 @@ const PixelWrapper = styled.div.attrs<IPixelWrapperProps>((props) => ({
   background: ${(props) => (props.isGameStarted ? "#567f35" : "grey")};
   border: 1px solid white;
   border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  ${(props) => {
+    // clip path for snake head
+    if (props.index === 0) {
+      switch (props.direction) {
+        case Direction.UP:
+          return css`
+            clip-path: polygon(20% 0%, 80% 0%, 100% 100%, 0% 100%);
+          `;
+        case Direction.DOWN:
+          return css`
+            clip-path: polygon(0 0, 100% 0, 80% 100%, 20% 100%);
+          `;
+        case Direction.RIGHT:
+          return css`
+            clip-path: polygon(0 0, 100% 20%, 100% 80%, 0% 100%);
+            flex-direction: column;
+          `;
+
+        case Direction.LEFT:
+          return css`
+            clip-path: polygon(0 20%, 100% 0, 100% 100%, 0 80%);
+            flex-direction: column;
+          `;
+        default:
+          return "";
+      }
+    }
+    return "";
+  }}
+`;
+
+const Eye = styled.div`
+  margin: 1px;
+  height: 3px;
+  width: 3px;
+  border-radius: 50%;
+  bottom: 5px;
+  left: 2px;
+  background: white;
+`;
+
+interface ITongueProps {
+  direction: Direction;
+  left: number;
+  bottom: number;
+}
+const Tongue = styled.div<ITongueProps>`
+  position: absolute;
+  background: red;
+  ${(props) => {
+    switch (props.direction) {
+      case Direction.UP:
+        return css`
+          height: 5px;
+          width: 2px;
+          left: ${props.left + 9}px;
+          bottom: ${props.bottom + 19}px;
+        `;
+      case Direction.DOWN:
+        return css`
+          height: 5px;
+          width: 2px;
+          left: ${props.left + 9}px;
+          bottom: ${props.bottom - 4}px;
+        `;
+      case Direction.RIGHT:
+        return css`
+          height: 2px;
+          width: 5px;
+          left: ${props.left + 19}px;
+          bottom: ${props.bottom + 9}px;
+        `;
+
+      case Direction.LEFT:
+        return css`
+          height: 2px;
+          width: 5px;
+          left: ${props.left - 5}px;
+          bottom: ${props.bottom + 9}px;
+        `;
+      default:
+        return "";
+    }
+  }}
 `;
 
 interface IPixelProps {
@@ -31,16 +120,31 @@ interface IPixelProps {
 }
 
 const Pixel = ({ positionX, positionY, index }: IPixelProps) => {
-  const { isStarted, pixelSize } = useGameContext();
+  const { isStarted, pixelSize, direction } = useGameContext();
+
+  const isHead = index === 0;
 
   return (
-    <PixelWrapper
-      index={index}
-      left={positionX}
-      bottom={positionY}
-      size={pixelSize}
-      isGameStarted={isStarted}
-    />
+    <>
+      <PixelWrapper
+        index={index}
+        left={positionX}
+        bottom={positionY}
+        size={pixelSize}
+        isGameStarted={isStarted}
+        direction={direction}
+      >
+        {isHead && (
+          <>
+            <Eye />
+            <Eye />
+          </>
+        )}
+      </PixelWrapper>
+      {isHead && isStarted && (
+        <Tongue direction={direction} left={positionX} bottom={positionY} />
+      )}
+    </>
   );
 };
 
